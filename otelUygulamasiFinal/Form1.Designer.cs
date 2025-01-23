6445987431c23959689268ae7e8fb2b4de8d8492
gokcetoprakal <168919531+gokcetoprakal@users.noreply.github.com>
2025-01-23 22:43:54 +0300

    Add files via upload

---
+++ otelUygulamasiFinal/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace otelUygulamasiFinal
+{
+    partial class form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(959, 503);
+            this.Name = "form1";
+            this.Text = "form1";
+            this.Load += new System.EventHandler(this.btn_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
